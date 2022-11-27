655d511afd2d9f4b8efee53d8fda30e57452e402
IvanJericevich <ivanjericevich96@gmail.com>
2022-11-27 21:30:49 +0200

    Start implementing clients API route

---
+++ database/src/models/mod.rs
@@ -0,0 +1,46 @@
+use sea_orm::prelude::*;
+use sea_orm::FromQueryResult;
+
+pub use crate::entities::{
+    clients::Model as Client, markets::Model as Market, sea_orm_active_enums::*,
+    sub_accounts::Model as SubAccount,
+};
+
+// ----------------------------------------------------------------------
+
+#[derive(Debug, FromQueryResult, PartialEq)]
+pub struct Order {
+    pub id: i32,
+    pub price: f32,
+    pub size: f32,
+    pub filled_size: Option<f32>,
+    pub side: OrderSide,
+    pub r#type: OrderType,
+    pub status: OrderStatus,
+    pub open_at: DateTime,
+    pub closed_at: Option<DateTime>,
+    pub sub_account_id: i32,
+    pub market_id: i32,
+    pub base_currency: String,
+    pub quote_currency: String,
+    pub price_increment: f32,
+    pub size_increment: f32,
+    pub sub_account: String,
+    pub client_id: i32,
+}
+
+#[derive(Debug, FromQueryResult, PartialEq)]
+pub struct Position {
+    pub id: i32,
+    pub avg_entry_price: f32,
+    pub size: f32,
+    pub side: OrderSide,
+    pub sub_account_id: i32,
+    pub market_id: i32,
+    pub base_currency: String,
+    pub quote_currency: String,
+    pub price_increment: f32,
+    pub size_increment: f32,
+    pub sub_account: String,
+    pub client_id: i32,
+}
